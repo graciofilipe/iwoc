2f261054f788b7272503a7d2ba063d1bc0a1c6f1
filipe.gracio <filipe.gracio@mclaren.com>
2019-11-06 15:47:00 +0000

    wip

--- QA.md
+++ QA.md
@@ -12,16 +12,15 @@ Orange
 2.1
 
 
-
 ##Which agent had the most signups? Which assumptions did you make? (note that there is a many-to-one relationship between calls and leads) [4]
-
-
-
-
-
+{'black': 65.83333333333336, 'orange': 308.4119047619046, 'green': 60.77142857142859, 'red': 286.93690476190466, 'blue': 46.046428571428585})
 
 
 ##Which agent had the most signups per call? [2]
+success rate (sing up credits divided by calls made)
+{'black': 0.08777777777777782, 'blue': 0.2313890882986361, 'green': 0.1792667509481669, 'orange': 0.13805367267766544, 'red': 0.19413863651008434}
+
+
 
 ##Was the variation between the agents’ signups-per-call statistically significant? Why? [5]
 
@@ -29,19 +28,19 @@ Orange
 
 ##A lead from which sector is most likely to be “interested” in the product? [1]
 
-##Given a lead has already expressed interest and signed up, 
+##Given a lead has already expressed interest and signed up: 
 
-##signups from which region are most likely to be approved? [2]
+###signups from which region are most likely to be approved? [2]
 
-##Is this statistically significant? Why? [5]
+###Is this statistically significant? Why? [5]
 
 ##Suppose you wanted to pick the 1000 leads most likely to sign up (who have not been called so far), based only on age, sector and region.
 
-##What criteria would you use to pick those leads? [10]
+###What criteria would you use to pick those leads? [10]
 
-##In what sense are those an optimal criteria set? [3]
+###In what sense are those an optimal criteria set? [3]
 
-##How many signups would you expect to get based on those called leads, assuming they were being called by random agents? [3]
+###How many signups would you expect to get based on those called leads, assuming they were being called by random agents? [3]
 
-##If you could choose the agents to make those calls, who would you choose? Why? [3]
+###If you could choose the agents to make those calls, who would you choose? Why? [3]
 
--- analysis_for_questions.py
+++ analysis_for_questions.py
@@ -2,7 +2,10 @@ import os
 from collections import defaultdict
 
 import pandas as pd
-
+import scipy as sp
+from scipy.stats import fisher_exact
+from scipy.stats import chi2_contingency
+from scipy.stats import chi2
 
 def further_exploration(calls, leads, signups, folder=None):
     leads_and_calls = calls.merge(leads, how='outer', on=['Phone Number'])
@@ -23,18 +26,52 @@ def further_exploration(calls, leads, signups, folder=None):
           total_number_of_calls / number_of_unique_called_numbers)
 
     # ##Which agent had the most signups? Which assumptions did you make?
-    calls_per_agent_per_number_df = calls_to_signed_up_numbers_df.groupby(by=['Phone Number', 'Agent']).count().drop(
+    calls_per_agent_per_sgined_number_df = calls_to_signed_up_numbers_df.groupby(by=['Phone Number', 'Agent']).count().drop(
         axis=1, labels=['Call Outcome']).rename({'Call Number': 'call_count_per_agent_per_number'}, axis=1)
     total_calls_to_number_df = calls_to_signed_up_numbers_df.groupby(by='Phone Number').count().drop(axis=1, labels=[
         'Call Outcome', 'Agent']).rename({'Call Number': 'call_count_per_number'}, axis=1)
     agent_sign_up_counts = defaultdict(int)
     for number in signed_up_phone_numbers_ls:
-        calls_to_number_by_agent = calls_per_agent_per_number_df.loc[number]
+        calls_to_number_by_agent = calls_per_agent_per_sgined_number_df.loc[number]
         total_calls_to_number = total_calls_to_number_df.loc[number][0]
         for agent in calls_to_number_by_agent.index:
             agent_sign_up_counts[agent] += calls_to_number_by_agent.loc[agent][
                                                0] / total_calls_to_number  # the contribution of the agents is the faction of calls they placed to the lead
     print('how many sign up counts per agent:', agent_sign_up_counts)
+
+    ##Which agent had the most signups per call?
+    number_of_calls_per_agent_df = calls.groupby(by=['Agent']).count().drop(
+        axis=1, labels=['Phone Number','Call Outcome']).rename({'Call Number': 'call_count_per_agent_per_number'}, axis=1)
+    success_rate_per_agent = defaultdict(int)
+    for agent in number_of_calls_per_agent_df.index:
+        success_rate_per_agent[agent] = agent_sign_up_counts[agent]/number_of_calls_per_agent_df.loc[agent][0]
+    print('the success rate per agent', success_rate_per_agent)
+
+    ## statiscal significant
+    number_of_calls_per_agent_ls = [number_of_calls_per_agent_df.loc[agent][0] for agent in number_of_calls_per_agent_df.index]
+    agent_sign_up_counts_ls = [int(agent_sign_up_counts[agent]) for agent in number_of_calls_per_agent_df.index]
+    table = [number_of_calls_per_agent_ls, agent_sign_up_counts_ls]
+    stat, p, dof, expected = chi2_contingency(table)
+    print('dof=%d' % dof)
+    print(expected)
+    # interpret test-statistic
+    prob = 0.95
+    critical = chi2.ppf(prob, dof)
+    print('probability=%.3f, critical=%.3f, stat=%.3f' % (prob, critical, stat))
+    if abs(stat) >= critical:
+        print('Dependent (reject H0)')
+    else:
+        print('Independent (fail to reject H0)')
+    # interpret p-value
+    alpha = 1.0 - prob
+    print('significance=%.3f, p=%.3f' % (alpha, p))
+    if p <= alpha:
+        print('Dependent (reject H0)')
+    else:
+        print('Independent (fail to reject H0)')
+
+    import ipdb; ipdb.set_trace()
+
 
 
 def further_explore():
