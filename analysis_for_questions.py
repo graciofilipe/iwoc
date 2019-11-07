5cd670ed9e98bdbf6b82821b74841d4f6fdce80a
filipe.gracio <filipe.gracio@mclaren.com>
2019-11-07 21:01:17 +0000

    logistic regression working

--- analysis_for_questions.py
+++ analysis_for_questions.py
@@ -7,7 +7,7 @@ from sklearn import linear_model
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix
-
+import scipy as sp
 
 #
 # def further_exploration(calls, leads, signups, folder=None):
@@ -127,9 +127,13 @@ def region_aproval_ratio_fun(leads, signups):
 ###How many signups would you expect to get based on those called leads, assuming they were being called by random agents? [3]
 ###If you could choose the agents to make those calls, who would you choose? Why? [3]
 
-def most_likely_leads_fun(leads, signups):
+def most_likely_leads_fun(leads, calls, signups):
 
-    leads_and_signups_df = leads.merge(signups, right_on='Lead', left_on='Name', how='left')
+    # redo this with leads that have been called only
+
+    calls_and_leads_df = calls.merge(leads, on='Phone Number', how='left')
+    leads_and_signups_df = calls_and_leads_df.merge(signups, right_on='Lead', left_on='Name', how='left')
+    leads_and_signups_df.drop_duplicates(subset=['Phone Number', 'Name', 'Approval Decision'], inplace=True)
     tmp_modelling_df = leads_and_signups_df[['Region', 'Sector',  'Age', 'Approval Decision']]
     tmp_modelling_df['Approval Decision'] = tmp_modelling_df['Approval Decision'].fillna(0)
     tmp_modelling_df.replace(to_replace='APPROVED', value=1, inplace=True)
@@ -138,32 +142,50 @@ def most_likely_leads_fun(leads, signups):
     region_encoder = OneHotEncoder()
     region_encoded = region_encoder.fit_transform(tmp_modelling_df.Region.values.reshape(-1, 1)).toarray()
     region_df = pd.DataFrame(data=region_encoded,
-                             columns=region_encoder.categories_,
+                             columns=[ca for ca in region_encoder.categories_[0]],
                              index=tmp_modelling_df.index.values)
     sector_encoder = OneHotEncoder()
     sector_encoded = sector_encoder.fit_transform(tmp_modelling_df.Sector.values.reshape(-1, 1)).toarray()
     sector_df = pd.DataFrame(data=sector_encoded,
-                             columns=sector_encoder.categories_,
+                             columns=[ca for ca in sector_encoder.categories_[0]],
                              index=tmp_modelling_df.index.values)
 
     X = pd.concat([sector_df, region_df], axis=1)
     X['age'] = tmp_modelling_df['Age']
     y = tmp_modelling_df['Approval Decision']
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.33, random_state=42)
-    logistic_reg = linear_model.LogisticRegressionCV()
-    sample_weights = [1 if outcome ==0 else 5 for outcome in y_train]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=666)
+    logistic_reg = linear_model.LogisticRegressionCV(fit_intercept=False)
+    n_pos = sum(y_train)
+    n_neg = sum(y_train==0)
+    sample_weights = [1/n_neg if outcome ==0 else 1/n_pos for outcome in y_train]
     logistic_reg.fit(X=X_train, y=y_train, sample_weight=sample_weights)
-    y_pred = logistic_reg.predict(X_test)
-    y_pred_bin = [0 if y <0.5 else 1 for y in y_pred]
+
+    print('results on TRAINING data')
+
+    y_pred_bin = logistic_reg.predict(X_train)
+    tn, fp, fn, tp = confusion_matrix(y_train, y_pred_bin).ravel()
+    print('tn', tn)
+    print('fp', fp)
+    print('fn', fn)
+    print('tp', tp)
+    print('accuracy', accuracy_score(y_pred=y_pred_bin, y_true=y_train))
+
+    print('results on TEST data')
+    y_pred_bin = logistic_reg.predict(X_test)
     tn, fp, fn, tp = confusion_matrix(y_test, y_pred_bin).ravel()
     print('tn', tn)
     print('fp', fp)
     print('fn', fn)
     print('tp', tp)
+    print('accuracy', accuracy_score(y_pred=y_pred_bin, y_true=y_test))
+
+
+    import ipdb; ipdb.set_trace()
+
+
+
 
     # dont forget to assert column order is the same
-    import ipdb; ipdb.set_trace()
     return 1
 
 
@@ -186,7 +208,7 @@ def further_explore():
     # region_interest_ratio_fun(leads, calls)
     # sector_interest_ratio_fun(leads, calls)
     # region_aproval_ratio_fun(leads, signups)
-    most_likely_leads_fun(leads, signups)
+    most_likely_leads_fun(leads, calls, signups)
 
 if __name__ == '__main__':
     further_explore()
